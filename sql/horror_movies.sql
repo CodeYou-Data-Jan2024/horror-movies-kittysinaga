a036a0729bd0ff0bfd82963c19ac29f7fee36ef1
Kitty Sinaga <kitty.sinaga@gmail.com>
2024-06-05 18:44:02 -0400

    Top 3 horror movies

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,9 @@
 -- Add your SQL here
+-- SELECT * FROM movies;
+
+SELECT m.id as 'Movie_ID', m.name as 'Movie_Title', m.imdb_rating as 'Rating'
+FROM movies m
+WHERE genre='horror' AND year <=1985
+ORDER BY imdb_rating DESC
+LIMIT 3;
+
